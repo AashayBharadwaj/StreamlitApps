425825ead75afc8309663840ba9c3972077548a9
Aashay Bharadwaj <aashay.bharadwaj@gmail.com>
2024-06-26 19:08:19 -0500

    Update app.py

--- Stocks-Charting/app.py
+++ Stocks-Charting/app.py
@@ -44,39 +44,39 @@ with pricing_data:
     st.write('Standard Deviation is ',stdev*100,'%')
     st.write('Risk Adj Return is', annual_return/(stdev*100))
     
-with fundamental_data:
-    key = API_KEY
-    fd=FundamentalData(key,output_format ='pandas')
-    st.subheader('Balance Sheet')
-    balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
-    bs = balance_sheet.T[2:]
-    bs.columns = list(balance_sheet.T.iloc[0])
-    st.write(bs)
+# with fundamental_data:
+#     key = API_KEY
+#     fd=FundamentalData(key,output_format ='pandas')
+#     st.subheader('Balance Sheet')
+#     balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
+#     bs = balance_sheet.T[2:]
+#     bs.columns = list(balance_sheet.T.iloc[0])
+#     st.write(bs)
     
-    st.subheader('Income Statement')
-    income_statement = fd.get_income_statement_annual(ticker)[0]
-    is1 = income_statement.T[2:]
-    is1.columns = list(income_statement.T.iloc[0])
-    st.write(is1)
+#     st.subheader('Income Statement')
+#     income_statement = fd.get_income_statement_annual(ticker)[0]
+#     is1 = income_statement.T[2:]
+#     is1.columns = list(income_statement.T.iloc[0])
+#     st.write(is1)
     
-    st.subheader('Cash Flow Statement')
-    cash_flow = fd.get_cash_flow_annual(ticker)[0]
-    cf = cash_flow.T[2:]
-    cf.columns = list(cash_flow.T.iloc[0])
-    st.write(cf)
-    st.write('Fundamental')
+#     st.subheader('Cash Flow Statement')
+#     cash_flow = fd.get_cash_flow_annual(ticker)[0]
+#     cf = cash_flow.T[2:]
+#     cf.columns = list(cash_flow.T.iloc[0])
+#     st.write(cf)
+#     st.write('Fundamental')
     
-with news:
-    st.header(f'News of {ticker}')
-    sn = StockNews(ticker, save_news=False)
-    df_news = sn.read_rss()
-    for i in range(10):
-        st.subheader(f'News {i+1}')
-        st.write(df_news['published'][i])
-        st.write(df_news['title'][i])
-        st.write(df_news['summary'][i])
-        title_sentiment = df_news['sentiment_title'][i]
-        st.write(f'Title Sentiment {title_sentiment}')
-        news_sentiment = df_news['sentiment_summary'][i]
-        st.write(f'News Sentiment {news_sentiment}')
+# with news:
+#     st.header(f'News of {ticker}')
+#     sn = StockNews(ticker, save_news=False)
+#     df_news = sn.read_rss()
+#     for i in range(10):
+#         st.subheader(f'News {i+1}')
+#         st.write(df_news['published'][i])
+#         st.write(df_news['title'][i])
+#         st.write(df_news['summary'][i])
+#         title_sentiment = df_news['sentiment_title'][i]
+#         st.write(f'Title Sentiment {title_sentiment}')
+#         news_sentiment = df_news['sentiment_summary'][i]
+#         st.write(f'News Sentiment {news_sentiment}')
     
