80a8160dc0a9f4e6b12d7d82ce84e37a7abb4620
Aashay Bharadwaj <aashay.bharadwaj@gmail.com>
2024-07-02 18:17:14 -0500

    Update data_processing.py

--- Stock-App-Testing/functions/data_processing.py
+++ Stock-App-Testing/functions/data_processing.py
@@ -1,13 +1,27 @@
 import plotly.express as px
 import numpy as np
-
+from datetime import datetime
+import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from .data_fetching import fetch_stock_data
+# from app import ticker,start_date,end_date
+
+
+
 
 def process_price_data(data, ticker):
     fig = px.line(data, x=data.index, y=data['Adj Close'], title=ticker)
     fig.update_yaxes(title_text=f"{ticker} Price")
     return fig
+
+
+# Default values
+default_ticker = 'MSFT'
+default_ticker_2 = 'No Selection'
+default_start_date = datetime(2020, 1, 1)
+default_end_date = datetime.today()
+
 
 def calculate_annual_return(data):
     return data['% Change'].mean() * 252 * 100
@@ -48,3 +62,31 @@ def plot_dual_y_axes(data1, ticker1, data2=None, ticker2=None):
     fig.update_yaxes(title_text=f"{ticker2} Price", secondary_y=True)
 
     return fig
+
+
+
+# # # Fetch stock data
+# data = fetch_stock_data(ticker, start_date, end_date)
+
+
+def process_data_and_metrics(data):
+    data2 = data.copy()
+    data2['% Change'] = data['Adj Close'] / data['Adj Close'].shift(1) - 1
+    data2.dropna(inplace=True)
+
+    annual_return = calculate_annual_return(data2)
+    stdev = calculate_standard_deviation(data2)
+    risk_adj_return = calculate_risk_adjusted_return(annual_return, stdev)
+
+    # Round values to two decimal places
+    annual_return_rounded = round(annual_return, 2)
+    stdev_rounded = round(stdev, 2)
+    risk_adj_return_rounded = round(risk_adj_return, 2)
+
+    # Display the rounded values
+    st.write('Annual Return is ', annual_return_rounded, '%')
+    st.write('Risk Adj Return is', risk_adj_return_rounded, '%')
+    
+    return data2, annual_return_rounded, stdev_rounded, risk_adj_return_rounded
+
+
