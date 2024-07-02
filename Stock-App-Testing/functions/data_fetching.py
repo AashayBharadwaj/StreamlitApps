454fad87e27a9590c0b9d6778d7bebae46f88f8e
Aashay Bharadwaj <aashay.bharadwaj@gmail.com>
2024-07-02 18:29:45 -0500

    Update data_fetching.py

--- Stock-App-Testing/functions/data_fetching.py
+++ Stock-App-Testing/functions/data_fetching.py
@@ -2,7 +2,7 @@ import pandas as pd
 import yfinance as yf
 
 def get_ticker_options():
-    ticker_data = pd.read_csv('../Stock-Tickers.csv')
+    ticker_data = pd.read_csv('./Stock-Tickers.csv')
     return ticker_data['Tickers'].tolist()
 
 def fetch_stock_data(ticker, start_date, end_date):
