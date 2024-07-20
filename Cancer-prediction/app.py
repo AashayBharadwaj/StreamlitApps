35f8bfc335d6af5fe9fd327663020cd482f2ef3d
Aashay Bharadwaj <aashay.bharadwaj@gmail.com>
2024-07-20 08:51:51 -0500

    Update app.py

--- Cancer-prediction/app.py
+++ Cancer-prediction/app.py
@@ -171,7 +171,7 @@ def main():
         st.title("Breast Cancer Predictor")
         st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar.")
     
-    col1, col2 = st.columns([4,1])
+    col1, col2 = st.columns([3,1])
     
     with col1:
         radar_chart = get_radar_chart(input_data)
