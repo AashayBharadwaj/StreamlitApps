74eba1faae8df1488d1ec52f22b3ef8fd5ef4397
Aashay Bharadwaj <aashay.bharadwaj@gmail.com>
2025-06-22 11:24:07 -0500

    Delete Stock-App-Testing directory

--- Stock-App-Testing/functions/data_fetching.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import yfinance as yf
-
-def get_ticker_options():
-    ticker_data = pd.read_csv('Stock-App-Testing/functions/Stock-Tickers.csv')
-    return ticker_data['Tickers'].tolist()
-
-def fetch_stock_data(ticker, start_date, end_date):
-    return yf.download(ticker, start=start_date, end=end_date)
